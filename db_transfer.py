f4fc2fd31902f96351c6e198f42dafd03d90cdec
pengzai <wengjinpeng@chupinxiu.com>
2018-12-17 00:02:09 +0800

    添加log

--- db_transfer.py
+++ db_transfer.py
@@ -248,7 +248,10 @@ class TransferBase(object):
 					db_instance.push_db_all_user()
 					logging.info('begin pull_db_all_user: %s',time.time())
 					rows = db_instance.pull_db_all_user()
-					logging.info('end pull_db_all_user: %s',time.time())
+					logging.info('end pull_db_all_user: %s  rows: %s',time.time(),len(rows))
+					if len(rows)<1:
+						logging.info('rows_count<0 跳过...')
+						continue
 					if rows:
 						db_instance.pull_ok = True
 						config = shell.get_config(False)
@@ -269,13 +272,13 @@ class TransferBase(object):
 				except Exception as e:
 					trace = traceback.format_exc()
 					logging.error(trace)
-					logging.error('db thread except:%s ' %(traceback.format_exc()))
-				if not ServerPool.get_instance().thread.is_alive():
-					logging.warn('pool is not alive --start')
-					break
-				if db_instance.event.wait(get_config().UPDATE_TIME):
-					logging.warn('event time --loop')
-					break
+					logging.error('dberror thread except:%s ' %(traceback.format_exc()))
+				else:
+					logging.error("dberror thread >>")
+				finally:
+					logging.info('thread_db pool')
+					if db_instance.event.wait(get_config().UPDATE_TIME):
+						logging.warn('db_instance.event.wait->>')
 		except KeyboardInterrupt as e:
 			pass
 		db_instance.del_servers()
@@ -387,18 +390,39 @@ class DbTransfer(TransferBase):
 					db=self.cfg["db"], charset='utf8',
 					ssl={'ca':self.cfg["ssl_ca"],'cert':self.cfg["ssl_cert"],'key':self.cfg["ssl_key"]})
 		else:
-			conn = cymysql.connect(host=self.cfg["host"], port=self.cfg["port"],
-					user=self.cfg["user"], passwd=self.cfg["password"],
-					db=self.cfg["db"], charset='utf8')
+			conn = self.reConndb()
 
 		try:
 			rows = self.pull_db_users(conn)
 		finally:
+			logging.warn('db close by  finally')
 			conn.close()
 
 		if not rows:
 			logging.warn('no user in db')
 		return rows
+
+	def reConndb(self):
+		import cymysql
+		# 数据库连接重试功能和连接超时功能的DB连接
+		_conn_status = True
+		_max_retries_count = 10  # 设置最大重试次数
+		_conn_retries_count = 0  # 初始重试次数
+		_conn_timeout = 3  # 连接超时时间为3秒
+		while _conn_status and _conn_retries_count <= _max_retries_count:
+			try:
+				print '连接数据库中..'
+				conn = cymysql.connect(host=self.cfg["host"], port=self.cfg["port"],
+					user=self.cfg["user"], passwd=self.cfg["password"],
+					db=self.cfg["db"], charset='utf8', connect_timeout=_conn_timeout)
+				_conn_status = False  # 如果conn成功则_status为设置为False则退出循环，返回db连接对象
+				return conn
+			except:
+				_conn_retries_count += 1
+				print _conn_retries_count
+			logging.warn('connect db is error!!')
+			#time.sleep(3)  # 此为测试看效果
+ 			continue
 
 	def pull_db_users(self, conn):
 		try:
